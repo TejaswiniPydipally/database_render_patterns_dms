afa3cc957900efe68be5e5e60af4ee1838a8cd15
TejaswiniPydipally <143117469+TejaswiniPydipally@users.noreply.github.com>
2024-04-17 16:06:33 -0500

    pushing project to repository

---
+++ DogsManagementSystem/src/main/java/com/example/Framework/DMS/repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+package com.example.Framework.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.Framework.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
